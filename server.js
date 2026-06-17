75346d70e662a937d220bc7f65946e442aed5bf2
adro <adro314159@gmail.com>
2026-06-17 18:32:12 +0200

    added pg-db

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const readline = require("readline");
 const path = require("path");
+const { Client, Pool } = require("pg");
+require("dotenv").config();
 
 const app = express();
 const router = express.Router();
@@ -12,6 +14,9 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+const usernameRegex = /^[a-zA-Z0-9_.-]+$/;
+
 
 app.use(express.json());
 
@@ -49,6 +54,58 @@ router.post("/api/getvoc", (req, res) => {
 
 
 
-app.listen(PORT, () => {
-    console.log(`Server läuft auf http://127.0.0.1:${PORT}`);
-});
+
+(async (password) => {
+    const pool = await initpg(password);
+    app.listen(PORT, () => {
+        console.log(`Server läuft auf http://127.0.0.1:${PORT}`);
+    });
+})(process.env.PG_Password);
+
+async function initpg(password) {
+    const client = new Client({
+        user: "postgres",
+        host: "localhost",
+        database: "postgres",
+        password: password,
+        port: 5432
+    });
+    try {
+        await client.connect();
+        console.log("Successfully connected to Postgres!")
+    } catch (err) {
+        console.log(`Error while logging into Postgres:\n  ${err}`);
+        process.exit(0);
+    }
+    
+    await client.query(`
+        CREATE DATABASE mclevdb
+        WITH OWNER = postgres
+    `).catch(err =>{
+        if (err.code !== "42P04") {
+            console.error(`Error while checking if DB already created:\n${err}`);
+            process.exit(0);
+        }
+    });
+
+    await client.end();
+
+    const pool = new Pool({
+        user: "postgres",
+        host: "localhost",
+        database: "mclevdb",
+        password: password,
+        port: 5432
+    });
+
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            username TEXT UNIQUE NOT NULL,
+            password_hash TEXT NOT NULL
+        )
+    `)
+
+    return pool;
+}
+
