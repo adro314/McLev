3e34fb0233fd8ec1e69e86ca0c9686e4b6a21a29
adro <adro314159@gmail.com>
2026-07-08 22:47:47 +0200

    login-page

--- frontend/src/app/pages/login-page/login-page.ts
+++ frontend/src/app/pages/login-page/login-page.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ThemeService } from '../../services/theme-service';
+import { InputField } from '../../components/input-field/input-field';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [InputField],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
-export class LoginPage {}
+export class LoginPage {
+  themeService = inject(ThemeService);
+}
