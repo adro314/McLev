3e19f8fec49b84732067ff87b636b8309aaef7d2
adro <adro314159@gmail.com>
2026-07-11 14:01:23 +0200

    form

--- frontend/src/app/components/input-field/input-field.ts
+++ frontend/src/app/components/input-field/input-field.ts
@@ -1,15 +1,47 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, forwardRef } from '@angular/core';
 import { ThemeService } from '../../services/theme-service';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input-field',
   imports: [],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputField),
+      multi: true
+    }
+  ],
   templateUrl: './input-field.html',
   styleUrl: './input-field.css',
 })
-export class InputField {
+export class InputField implements ControlValueAccessor {
   themeService = inject(ThemeService);
 
   name = input.required<string>();
   type = input.required<string>();
+
+  value = '';
+
+  private onChange = (value: string) => {};
+  onTouched = () => {};
+
+  writeValue(value: string): void {
+    this.value = value ?? '';
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  onInput(event: Event){
+    const value = (event.target as HTMLInputElement).value;
+
+    this.value = value;
+    this.onChange(value);
+  }
 }
--- frontend/src/app/pages/login-page/login-page.ts
+++ frontend/src/app/pages/login-page/login-page.ts
@@ -2,13 +2,27 @@ import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../services/theme-service';
 import { InputField } from '../../components/input-field/input-field';
 import { InputButton } from '../../components/input-button/input-button';
+import { ApiService } from '../../services/api-service';
+import { FormBuilder,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [InputField, InputButton],
+  imports: [InputField, InputButton,ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
   themeService = inject(ThemeService);
+  apiService = inject(ApiService);
+
+  private fb = inject(FormBuilder);
+
+  loginForm = this.fb.group({
+    username: [''],
+    password: ['']
+  })
+
+  login(){
+    console.log(this.loginForm.value);
+  }
 }
