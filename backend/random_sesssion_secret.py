80c5c461a2f6dbba316233729b2c7eca6ba1cb8a
adro <adro314159@gmail.com>
2026-07-19 15:05:49 +0200

    better secret-generator

--- backend/random_sesssion_secret.py
+++
@@ -1,3 +0,0 @@
-import random
-chars = [chr(i) for i in range(ord("a"),ord("z")+1)] + [chr(i) for i in range(ord("A"),ord("Z")+1)] + [chr(i) for i in range(ord("0"),ord("9")+1)] + [".","-"]
-print("".join([chars[random.randint(0,len(chars)-1)] for i in range(128)]))
