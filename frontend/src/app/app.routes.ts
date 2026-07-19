05ec21aa1941fe0072ca105272987bc62cfcf770
adro <adro314159@gmail.com>
2026-07-19 14:51:25 +0200

    Website-Titles

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomePage } from './pages/home-page/home-page';
 import { LoginPage } from './pages/login-page/login-page';
 
 export const routes: Routes = [
-    { path: '', component: HomePage},
-    { path: 'login', component: LoginPage}
+    { path: '', component: HomePage, title: "Vocabulary Trainer"},
+    { path: 'login', component: LoginPage, title: "Login"}
 ];
